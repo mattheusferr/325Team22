1c4fa6069e64c7b08232d9e1887470ff0fdb8c23
Duong <167362219+duong0914@users.noreply.github.com>
2024-04-20 20:30:05 -0400

    Add files via upload

---
+++ prerequisites/index.js
@@ -0,0 +1,4 @@
+function toggleBottomRight() {
+    var bottomRight = document.querySelector('.bottom-right');
+    bottomRight.style.display = (bottomRight.style.display === 'none') ? 'block' : 'none';
+}
