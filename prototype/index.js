37208eb2700c0f4adc8d26cb115c1c2a46d3a441
Duong <167362219+duong0914@users.noreply.github.com>
2024-04-21 16:42:08 -0400

    Delete prototype directory

--- prototype/index.js
+++
@@ -1,8 +0,0 @@
-function toggleDetail(detailBoxId) {
-    var detailBox = document.getElementById(detailBoxId);
-    if (detailBox.style.display === "" || detailBox.style.display === "none") {
-        detailBox.style.display = "block";
-    } else {
-        detailBox.style.display = "none";
-    }
-}
