fbef56d6f41633f4199a9357fdff0702e3ca90e2
Duong <167362219+duong0914@users.noreply.github.com>
2024-04-21 00:41:39 -0400

    Delete prerequisites directory

--- prerequisites/index.js
+++
@@ -1,4 +0,0 @@
-function toggleBottomRight() {
-    var bottomRight = document.querySelector('.bottom-right');
-    bottomRight.style.display = (bottomRight.style.display === 'none') ? 'block' : 'none';
-}
